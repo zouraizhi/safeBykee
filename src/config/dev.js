fbad9cfdb5b6eac9f2a24dbea30cf912ecec2ebb
HiZouraiz <48647109+HiZouraiz@users.noreply.github.com>
2020-09-09 22:01:23 +0500

    initial commit

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,11 +1,11 @@
 export const FirebaseConfig = {
-  apiKey: "AIzaSyCjRqe8-b1MVaT9Es7xejCD_6qPab32obM",
-  authDomain: "uberclone-fcbf6.firebaseapp.com",
-  databaseURL: "https://uberclone-fcbf6.firebaseio.com",
-  projectId: "uberclone-fcbf6",
-  storageBucket: "uberclone-fcbf6.appspot.com",
-  messagingSenderId: "826759570510",
-  appId: "1:826759570510:web:883c8a14f57f9596494f1f",
-  measurementId: "G-M3BRXVMHXW"
+  apiKey: "AIzaSyC5OFKADIfA84VnOA5Ct3krxiE9qB6DNzM",
+  authDomain: "safebykee-7810e.firebaseapp.com",
+  databaseURL: "https://safebykee-7810e.firebaseio.com",
+  projectId: "safebykee-7810e",
+  storageBucket: "safebykee-7810e.appspot.com",
+  messagingSenderId: "192487799053",
+  appId: "1:192487799053:web:36204e153a4cd408885844",
+  measurementId: "G-CH2V8RMZLW"
 };
 
